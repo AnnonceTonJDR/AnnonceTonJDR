f9a425f677756000bca8d8cad295743101cd6a95
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-19 11:41:25 +0200

    Ajout d'un eventuel model pour la BD en lecture unique

--- app/model/BD_lecture.php
+++ app/model/BD_lecture.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Permet la connexion à la base de données en écriture, et via un singleton
+ * Permet la connexion à la base de données en lecture uniquement, et via un singleton
  *
  * @author Lucas OMS
  *
  */
-class BD
+class BD_lecture
 {
     private static $bdd = null;
 
@@ -30,7 +30,7 @@ class BD
     {
         $pdo = null;
         try {
-            $pdo = new PDO("mysql:host=host;dbname=name;charset=utf8", BD::$user, BD::$pass);
+            $pdo = new PDO("mysql:host=host;dbname=databasename;charset=utf8", BD_lecture::$user, BD_lecture::$pass);
             if (isset($pdo))
                 self::$bdd = $pdo;
         } catch (Exception $e) {
@@ -42,10 +42,10 @@ class BD
     /**************************************************************
      *Fonction statique pour récupèrer l'unique connexion à la base de donnée    *
      ***************************************************************/
-    public static function connexionBDD()
+    public static function connexionBDD_lecture()
     {
         if (self::$bdd == null) {
-            new BD();
+            new BD_lecture();
         }
         return self::$bdd;
     }
