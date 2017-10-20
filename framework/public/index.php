d02ca76d3c4b8e6e51453f57919636276f6932a2
PIARDSylvian <piard.sylvian@gmail.com>
2017-10-20 14:53:03 +0200

    structure

--- framework/public/index.php
+++ framework/public/index.php
@@ -4,3 +4,4 @@ require '../vendor/autoload.php';
 $app = new \Framework\App();
 
 $response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
+\Http\Response\send($response);
--- framework/tests/Framework/AppTest.php
+++ framework/tests/Framework/AppTest.php
@@ -16,4 +16,22 @@ class AppTest extends TestCase {
         $this->assertContains('/demoslash', $response->getHeader('Location'));
         $this->assertEquals(301, $response->getStatusCode());
     }
+
+    public function testBlog() {
+        $app = new App();
+        $request = new ServerRequest('GET', '/blog');
+        $response = $app->run($request);
+
+        $this->assertContains('<h1>Bienvenue sur le blog</h1>', (string)$response->getBody());
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testError404() {
+        $app = new App();
+        $request = new ServerRequest('GET', '/aze');
+        $response = $app->run($request);
+
+        $this->assertContains('<h1>Erreur 404</h1>', (string)$response->getBody());
+        $this->assertEquals(404, $response->getStatusCode());
+    }
 }
