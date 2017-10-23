50d222c737f1d0b02b4711d554d99f5387a7d709
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-23 21:25:00 +0200

    Ajout de session.php utile à tous les controleurs de sessions

---
+++ app/model/session.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************
+ *Créateur:        Alexis POYEN                            *
+ *Date:                14/04/17                                    *
+ *Description:    Classe pour gérer les sessions        *
+ ***************************************/
+
+include_once 'Utilisateur.php';
+
+class Session
+{
+    private $utilisateur;
+
+    /**
+     * @author Lucas OMS
+     * @version
+     * @param $utilisateur
+     */
+    public function __construct($utilisateur)
+    {
+        $this->utilisateur = $utilisateur;
+    }
+
+    public function getUtilisateur(): Utilisateur
+    {
+        return $this->utilisateur;
+    }
+}
