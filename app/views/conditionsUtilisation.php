1bf1cb1e25bb532436139a3cdc1b19ec0213dd7a
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-23 22:47:34 +0200

    ajout des CGU pour Amir

---
+++ app/views/conditionsUtilisation.php
@@ -0,0 +1,334 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Gueldar - CGU</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/presentationAccueil.css">
+    <link rel="stylesheet" href="/css/vitrine.css">
+    <link rel="stylesheet" href="/css/conditionUtilisation.css">
+    <script src="/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
+        href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!-- Add your site or application content here -->
+<div id="wrapper">
+    <?php include_once 'utils/accueilHeader.php' ?>
+    <div id="contenu" class="CGU">
+        <h1 id="CGU">Conditions générales d'utilisations - Gueldar</h1>
+        <div class="titre">
+            <img src="/img/site/titreCGU.png">
+            <h2>Présentation</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>1.1</strong> Les présentes Conditions Générales d'Utilisation (ci-après les <strong>«&nbsp;CGU&nbsp;»</strong>)
+                énoncent les conditions d'utilisation des services fournis par Gueldar,
+                projet en équipe à but non lucratif ci-après individuellement ou conjointement désignés
+                <strong>«&nbsp;Gueldar&nbsp;»</strong>,
+                l'<strong>«&nbsp;Équipe&nbsp;»</strong> ou <strong>«&nbsp;Nous&nbsp;»</strong>).<br><br>
+
+                <strong>1.2</strong> Certains services ne nécessite pas de connection et vous place en <strong>
+                    «&nbsp;visiteur&nbsp;»</strong>, le reste des services que Nous proposons vous place en
+                <strong>«&nbsp;utilisateur&nbsp;»</strong>.<br>
+
+                La consultation, l'utilisation du jeu, les contacts et toutes fonctionnalités du site sont
+                désignés
+                collectivement par le terme «&nbsp;Services&nbsp;».<br><br>
+
+                <strong>1.3</strong> Ainsi, les CGU déterminent le cadre contractuel des relations entre
+                l'utilisateur
+                des Services (ci-après <strong>«&nbsp;vous&nbsp;»</strong>, le
+                <strong>«&nbsp;visiteur&nbsp;»</strong> ou l'<strong>«&nbsp;utilisateur&nbsp;»</strong>) et Gueldar.<br><br>
+            </p>
+        </div>
+        <div class="titre">
+            <img src="/img/site/titreCGU.png">
+            <h2>Acceptation des CGU</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>2.1</strong> Vous reconnaissez que vous êtes une personne physique ayant atteint l'âge de la
+                majorité dans votre pays de résidence habituelle ou, si vous êtes mineur et que la loi de votre pays
+                de
+                résidence habituelle l’exige, avoir obtenu l'autorisation de votre représentant légal (parent ou
+                tuteur)
+                pour créer un Compte et/ou accéder aux Services. Vous reconnaissez également que vous êtes en
+                capacité de passer un contrat valable conformément à la loi, la jurisprudence ou aux usages de votre
+                pays
+                de résidence habituelle.<br><br>
+                <strong>2.2</strong> Toute utilisation des Services non conforme aux CGU est prohibée.<br><br>
+                <strong>2.3</strong> L’acceptation des CGU est matérialisée par votre signature électronique,
+                concrétisée par le fait de cocher la case prévue à cet effet.
+                Cette acceptation est requise pour créer un Compte et/ou utiliser les Services et/ou
+                participer au Jeu. En cas de désaccord avec ces dispositions, vous ne devez pas cocher la
+                case ou cliquer sur le bouton d’acceptation des CGU. En cas de mise à jour des CGU, il sera demandé
+                aux
+                utilisateurs de marquer leur accord par le biais d’une case à cocher ou d’un clic sur un bouton
+                d’acceptation des CGU, faute de quoi ils ne pourront plus accéder aux Services.<br><br>
+            </p>
+        </div>
+        <div class="titre">
+            <img src="/img/site/titreCGU.png">
+            <h2>Acces au site</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>3.1</strong> Les Services étant proposés sur Internet, pour y accéder, vous devez avoir
+                une connexion Internet. Tous les coûts des connexions téléphoniques et d'accès à Internet sont à
+                votre charge. Nous ne sommes pas responsable des dommages que vous pourriez subir suite à
+                l'utilisation d'une connexion Internet ou de l'installation sur votre ordinateur de logiciels
+                malveillants.<br><br>
+                <strong>3.2</strong> Certains Services sont accessibles via un appareil mobile. On entend par
+                «&nbsp;appareil mobile&nbsp;» tout appareil portable capable de se connecter à Internet pour accéder
+                auxdits Services (téléphones mobiles, tablettes tactiles, assistants numériques personnels …).
+                Vous reconnaissez que la qualité des Services, le délai de réponse ou l’accès à certaines
+                fonctionnalités peuvent
+                dépendre des capacités de votre appareil mobile et du réseau de communication électronique.
+                Vous reconnaissez que les Services proposés par Gueldar sur appareil mobile peuvent ne pas être
+                disponibles sur tous
+                les appareils mobiles ou certaines résolutions d'écran.<br><br>
+                <strong>3.3</strong> Pour accéder aux Services et/ou à l’ensemble des fonctionnalités des
+                Services, l'Association pourra vous demander de créer un compte Gueldar (ci-après le <strong>«&nbsp;Compte&nbsp;»</strong>).
+                Vous pouvez créer un Compte à partir des encarts prévus à cet effet sur le
+                site
+                internet de Gueldar (ci-après le <strong>«&nbsp;Site&nbsp;»</strong>), se trouvant dans le
+                formulaire de connexion
+                après l'appuie du bouton <strong>«&nbsp;JOUER&nbsp;»</strong>, en suivant <strong>«&nbsp;S'inscrire&nbsp;»</strong>.<br><br>
+            </p>
+        </div>
+        <div class="titre">
+            <img src="/img/site/titreCGU.png">
+            <h2>Creation de compte</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>4.1.1</strong> Pour créer un Compte, vous devez être une personne physique ayant atteint
+                l'âge légal dans votre pays de résidence habituelle ou, si vous êtes mineur et que la loi de votre
+                pays de résidence habituelle l’exige, avoir obtenu l'accord de votre représentant légal (parent ou
+                tuteur), et, en tout état de cause, être légalement en capacité de contracter avec
+                l'Équipe.<br><br>
+                <strong>4.1.2</strong> Le Compte est un compte utilisateur mis à votre disposition via un nom de
+                Compte et un mot de passe.<br><br>
+                <strong>4.1.3</strong> Le Compte reste la propriété de l'Équipe. Vous n'êtes titulaire que
+                d'un droit d'accès aux Services, par l'intermédiaire d'un Compte mis à votre disposition.<br><br>
+                <strong>4.1.4</strong> Pour créer un Compte, vous devrez entrer les informations suivantes :
+            </p>
+            <ul>
+                <li>nom et prénom ;</li>
+                <li>pseudonyme ;</li>
+                <li>adresse e-mail;</li>
+                <li>mot de passe ;</li>
+            </ul>
+            <p>
+                <strong>4.1.5</strong>Le nom et prénom Nous permettent de vous restituer un accès à un compte perdu
+                ou bloqué par la justification de votre identité.
+                Le pseudonyme ou l'adresse mail couplé au mot de passe vous permet d'accéder
+                aux Services. L’adresse e-mail Nous permet de communiquer avec vous dans le cadre de la gestion et
+                du suivi de votre Compte et de vous envoyer des informations relatives aux Services.<br><br>
+                <strong>4.2.1</strong> Le pseudonyme n'est pas visible par les autres utilisateurs. <br><br>
+                <strong>4.2.2</strong> Le pseudo doit respecter au minimum les règles suivantes :
+            </p>
+            <ul>
+                <li>correspondre à un mot prononçable (par exemple le mot ssdfzxjf ne respecte pas cette règle) ;
+                </li>
+                <li>ne pas faire référence à une orientation politique, à une ethnie, une communauté, une religion ;
+                </li>
+                <li>ne pas être vulgaire ou insultant ;</li>
+                <li>ne pas avoir de connotation sexuelle voire pornographique ;</li>
+                <li>ne pas contenir votre prénom (les pseudos avec un prenom suivi d'un numéro ne seront pas
+                    autorisés);
+                </li>
+                <li>ne pas ressembler ou imiter une marque déposée ou un terme protégé par le droit d’auteur ;
+                </li>
+                <li>ne pas faire la promotion d'un service commercial ;</li>
+                <li>ne pas faire référence à un produit stupéfiant ou toute autre entité interdite par la
+                    législation ;
+                </li>
+                <li>ne pas ressembler ou imiter le nom d'un modérateur ou d'un membre ayant droit au sein de
+                    l'Association ;
+                </li>
+                <li>ne pas être orthographié ou épelé alternativement dans le but de contourner les règles
+                    imposées ci-dessus ;
+                </li>
+            </ul>
+            <p>
+                L’appréciation du respect ou du non-respect de ces règles (notamment l’appréciation de la
+                ressemblance entre un pseudo et un autre terme) est laissée à la libre discrétion de
+                l'Équipe.<br>
+                L'Équipe se réserve le droit de modifier sans avertissement ou justification un pseudo qui ne
+                respecterait pas les contraintes précedemments définies, le nouveau pseudo sera alors choisi par un
+                membre de l'Équipe parmis un pannel défini.
+                <br><br>
+            </p>
+        </div>
+        <div class="titre">
+            <img src="/img/site/titreCGU.png">
+            <h2>Support</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>5.1</strong> Le Support Gueldar mettra tout en œuvre pour vous assister dans les
+                meilleurs délais. Néanmoins, le Support se réserve le droit de ne pas répondre à une demande,
+                notamment en cas de non-respect des règles du présent article.<br><br>
+                <strong>5.2</strong> Tout harcèlement, menace, insulte ou autre comportement inadéquat à l’égard
+                des conseillers du Support entraineront un arrêt du traitement de la demande et pourront donner lieu
+                à des sanctions allant jusqu'à des poursuites judiciaires selon la gravité de l'acte.<br><br>
+                <strong>5.3</strong> En cas d’usurpation d’identité et/ou de falsification de documents et/ou de
+                tentative de l’une ou l’autre de ces infractions, le Support se réserve le droit de sanctionner
+                le/les Compte/s appartenant au fraudeur.<br><br>
+                <strong>5.4</strong> Le Support fournit une assistance pour des problèmes impactant
+                l’accessibilité et/ou le confort d’utilisation du visiteur ou de l'utilisateur mais n’a pas vocation
+                à répondre à des questions autres que technique, même ayant un quelconque rapport avec le contenu du
+                Site.<br><br>
+            </p>
+        </div>
+        <div class="titre">
+            <img src="/img/site/titreCGU.png">
+            <h2>Copyright</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>6.1</strong> Toutes les images, documents, vidéos sont protégés par les lois françaises
+                et internationales sur le droit d'auteur et la propriété intellectuelle. Tous ces éléments,
+                fonctionnalités, outils, documents appartiennent à Gueldar. Sauf autorisation écrite
+                préalable de Gueldar, vous vous engagez à ne pas faire le commerce d’éléments issus des créations de
+                Gueldar et à ne pas faire la promotion de ce genre d’activités portant atteinte aux droits
+                de Gueldar. Gueldar mettra tout en œuvre pour empêcher de tels actes.<br><br>
+                <strong>6.2</strong> Toutes données liées au Site ou Services appartiennent à Gueldar et
+                sont protégées par les lois françaises, européennes et internationales sur le droit d'auteur. Vous
+                n'avez pas le droit, en tout ou partie, de copier, reproduire, traduire, extraire, modifier le code
+                source,
+                désassembler, décompiler, modifier, louer, vendre, distribuer ou créer des œuvres dérivées
+                inspirées du Site de Gueldar sans notre accord préalable.<br><br>
+                <strong>6.4</strong> Toute utilisation non conforme met fin à l'autorisation ou à la licence
+                accordée par Gueldar<br><br>
+            </p>
+        </div>
+        <div class="titre" id="donneesPersonnelles">
+            <img src="/img/site/titreCGU.png">
+            <h2>Données personnelles</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                <strong>7.1</strong> Vous reconnaissez que vous êtes en mesure de Nous communiquer vos données à
+                caractère personnel conformément à la loi du pays dans lequel vous avez votre résidence
+                habituelle.<br><br>
+                <strong>7.2</strong> Les informations personnelles recueillies font l'objet d'un traitement
+                informatique destiné à permettre de vous contacter dans le cadre du suivi de vos commandes ou de
+                manière générale de gérer la relation client, de vérifier votre identité lors d'échanges avec le
+                Support ou encore de vous rendre l'accès à un compte.<br><br>
+
+                Conformément à la loi "Informatique et libertés" du 6 janvier 1978, vous bénéficiez d'un droit
+                d'accès et de rectification aux informations qui vous concernent, que vous pouvez exercer via le
+                Support vos noms, prénoms, adresse e-mail et en justifiant de votre identité.<br>
+                Vous pouvez également, pour des motifs légitimes, vous opposer au traitement des données vous
+                concernant.<br><br>
+                <strong>7.3</strong> L'Équipe s'engage à ne pas communiquer vos informations à des tiers et
+                à tout mettre en œuvre pour empêcher la diffusion de celles-ci, sauf autorisation expresse de votre
+                part. Gueldar se réserve le droit de poursuivre toute personne qui tenterait d'accéder à des
+                informations personnelles qui ne la concernerait pas.<br><br>
+                <strong>7.5</strong> L'Équipe n'a aucune obligation de fournir des informations liées à un
+                Compte spécifique, même si un utilisateur le demande, sauf aux autorités compétentes dans le cadre
+                d'une enquête judiciaire.<br><br>
+            </p>
+        </div>
+        <div class="titre" id="cookies">
+            <img src="/img/site/titreCGU.png">
+            <h2>Cookies</h2>
+        </div>
+        <div class="mentionsLégales">
+            <p>
+                Lorsqu’une personne visite nos Sites, des cookies ou fichiers similaires sont susceptibles
+                d’être installés notamment sur le disque dur de l’ordinateur de l’internaute
+            </p>
+            <h3>Qu'est-ce qu'un cookie ?</h3>
+            <p>
+                Un cookie est une information déposée sur votre disque dur par le serveur du site que vous
+                visitez.<br>
+                Il contient plusieurs données :
+            </p>
+            <ul>
+                <li>le nom du serveur qui l'a déposée ;</li>
+                <li>un identifiant sous forme de numéro unique ;</li>
+                <li>éventuellement une date d'expiration…</li>
+            </ul>
+            <p>
+                Ces informations sont parfois stockées sur votre ordinateur dans un simple fichier texte auquel
+                un
+                serveur
+                accède pour lire et enregistrer des informations.<br>
+                Vous trouverez plus d’informations sur les cookies sur le <a
+                        href="//www.cnil.fr/vos-droits/vos-traces/les-cookies/">site de la CNIL</a> et sur le
+                site
+                <a href="//www.allaboutcookies.org/">allaboutcookies.org</a>.<br><br>
+
+                <strong>8.1</strong> En naviguant sur notre Site, vous acceptez l'utilisation du cookie Google
+                Analytics (fourni par Google Inc.) nécessaire à des fins statistiques et pour d'éventuelles
+                améliorations de nos Services.<br><br>
+            </p>
+        </div>
+        <h1 id="mentionsLegales">Mentions légales - Gueldar</h1>
+        <div class="mentionsLégales center">
+            <h3>Editeur</h3>
+            <p>
+                Gueldar <br>
+                Projet à but non lucratif<br>
+                Coordonnées du chef de projet&nbsp;:<br>
+                136 Boulevard Chave<br>
+                13005 Marseille<br>
+                Téléphone (contact du projet)&nbsp;: 06&nbsp;21&nbsp;83&nbsp;54&nbsp;36<br>
+                Email&nbsp;: lucas.oms@hotmail.fr<br><br>
+            </p>
+            <h3>Directeur de la publication</h3>
+            <p>Monsieur Lucas OMS</p>
+
+            <h3>Hébergement</h3>
+            <p>
+                Serveur alwaysdata<br>
+                Détenteur du compte :<br>
+                Lucas OMS<br>
+                lucas.oms@hotmail.fr<br>
+                <br><br><br>
+            </p>
+        </div>
+    </div>
+    <?php include_once 'utils/accueilFooter.php' ?>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="/js/plugins.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function (b, o, i, l, e, r) {
+        b.GoogleAnalyticsObject = l;
+        b[l] || (b[l] =
+            function () {
+                (b[l].q = b[l].q || []).push(arguments)
+            });
+        b[l].l = +new Date;
+        e = o.createElement(i);
+        r = o.getElementsByTagName(i)[0];
+        e.src = 'https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e, r)
+    }(window, document, 'script', 'ga'));
+    ga('create', 'UA-XXXXX-X', 'auto');
+    ga('send', 'pageview');
+</script>
+</body>
+</html>
