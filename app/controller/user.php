c490161d1ede3628c54591b7dd706699088fc6d0
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-25 07:52:07 +0200

    Ajout des controlleurs qui seront utilisés à priori

---
+++ app/controller/user.php
@@ -0,0 +1,67 @@
+<?php
+
+/* 
+ page:          index
+ version:       1.0
+ arguments:     none
+ return:        viod
+*/
+function index()
+{
+    return "je suis la page index";
+}
+
+/* 
+ page:          register
+ version:       1.0
+ arguments:     none
+ return:        viod
+*/
+function register()
+{
+    return "je suis la page register";
+}
+
+/* 
+ page:          login
+ version:       1.0
+ arguments:     none
+ return:        viod
+*/
+function login()
+{
+    return "je suis la page login";
+}
+
+/* 
+ page:          user
+ version:       1.0
+ arguments:     none
+ return:        viod
+*/
+function user()
+{
+    return "je suis la page de gestion de profil";
+}
+
+/* 
+ page:          cgu
+ version:       1.0
+ arguments:     none
+ return:        viod
+*/
+function cgu()
+{
+    return "je suis la page des CGU";
+}
+
+/* 
+ page:          req
+ version:       1.0
+ arguments:     none
+ return:        viod
+*/
+function req()
+{
+    return "je suis la requette";
+}
