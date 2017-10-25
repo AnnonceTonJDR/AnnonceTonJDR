8f9402d3c52577cf86567e9aaf4a8257fe6d778a
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-25 08:01:37 +0200

    Ajout de la phpdoc sur les controlleurs

--- app/controller/register.php
+++ app/controller/register.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Controlleur de la page d'inscription
+ *
+ * @author Lucas OMS
+ * @version 1
+ */
 
 //IF everything ok :
 include_once '../views/register.php';
--- app/controller/user.php
+++ app/controller/user.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Controlleur de la page d'affichage d'un profil
+ *
+ * @author Lucas OMS
+ * @version 1
+ */
 
 //IF everything ok :
 include_once '../views/user.php';
