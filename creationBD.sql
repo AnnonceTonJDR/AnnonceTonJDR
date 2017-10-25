d05dc4dc6975d5ab89d686680c10c1239c4aff53
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-25 11:16:49 +0200

    Ajout de la clé étrangère à la table UtilisateurPrivate

--- creationBD.sql
+++ creationBD.sql
@@ -1,17 +1,30 @@
 CREATE TABLE IF NOT EXISTS `Utilisateur` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(32) NOT NULL,
-  `prenom` varchar(32) NOT NULL,
-  `pseudo` varchar(32) NOT NULL,
-  `dateInscription` date NOT NULL,
+  `id`              INT(11)     NOT NULL,
+  `nom`             VARCHAR(32) NOT NULL,
+  `prenom`          VARCHAR(32) NOT NULL,
+  `pseudo`          VARCHAR(32) NOT NULL,
+  `dateInscription` DATE        NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=0;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  AUTO_INCREMENT = 0;
 
 CREATE TABLE IF NOT EXISTS `UtilisateurPrivate` (
-  `id` int(11),
-  `mail` varchar(255) NOT NULL,
-  `etat` int(11) NOT NULL,
-  `motDePasse` varchar(64) NOT NULL,
-  `sel` varchar(32) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=0 ;
+  `id`         INT(11),
+  `mail`       VARCHAR(255) NOT NULL,
+  `etat`       INT(11)      NOT NULL,
+  `motDePasse` VARCHAR(64)  NOT NULL,
+  `sel`        VARCHAR(32)  NOT NULL
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  AUTO_INCREMENT = 0;
+
+ALTER TABLE `UtilisateurPrivate`
+  ADD FOREIGN KEY (`id`) REFERENCES `lucasoms_annoncetonjdr`.`Utilisateur` (
+  `id`
+)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
