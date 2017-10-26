c6c5d8d575622e390f89ea62e26f92cceb7348b2
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-26 11:48:05 +0200

    Modification du titre de la page

--- app/views/register.php
+++ app/views/register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Annonce ton JDR</title>
+    <title>Annonce ton JDR - Inscription</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
