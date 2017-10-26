57b559b7a13805ea1f17d7c1a73f7eaeb992f433
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-26 12:02:14 +0200

    Ajout de la prise en comtep de la touche entrée pour valider le formulaire

--- web/js/site/register.js
+++ web/js/site/register.js
@@ -12,69 +12,75 @@ function erreurCritique() {
     );
 }
 
-$(document).ready(function () {
-    $("#boutonValider").click(function () {
-        $.ajax({
-            type: 'post',
-            url: '/app/controller/connexion/inscription.php',
-            data: {
-                'nom': $('#nom').val(),
-                'prenom': $('#prenom').val(),
-                'pseudo': $('#pseudo').val(),
-                'mail': $('#mail').val(),
-                'motDePasse': $('#motDePasse').val(),
-                'motDePasseConfirmation': $('#motDePasseConfirmation').val()
-            }
-        }).done(function (data) {
-            if (data.ok) {
-                $('#erreur').hide();
-                //TODO Afficher l'activation par mail requise
-                //TODO Un bouton pour revnir à l'accueil explicitement
+function envoyerFormulaireInscription() {
+    $.ajax({
+        type: 'post',
+        url: '/app/controller/connexion/inscription.php',
+        data: {
+            'nom': $('#nom').val(),
+            'prenom': $('#prenom').val(),
+            'pseudo': $('#pseudo').val(),
+            'mail': $('#mail').val(),
+            'motDePasse': $('#motDePasse').val(),
+            'motDePasseConfirmation': $('#motDePasseConfirmation').val()
+        }
+    }).done(function (data) {
+        if (data.ok) {
+            $('#erreur').hide();
+            //TODO Afficher l'activation par mail requise
+            //TODO Un bouton pour revnir à l'accueil explicitement
+        }
+        else {
+            // console.log({
+            //     'nom': $('#nom').val(),
+            //     'prenom': $('#prenom').val(),
+            //     'pseudo': $('#pseudo').val(),
+            //     'mail': $('#mail').val(),
+            //     'motDePasse': $('#motDePasse').val(),
+            //     'motDePasseConfirmation': $('#motDePasseConfirmation').val()
+            // });
+            if (data.pseudo) {
+                $('#inputPseudo').removeClass("successForm").remove("errorForm").addClass("successForm");
             }
             else {
-                // console.log({
-                //     'nom': $('#nom').val(),
-                //     'prenom': $('#prenom').val(),
-                //     'pseudo': $('#pseudo').val(),
-                //     'mail': $('#mail').val(),
-                //     'motDePasse': $('#motDePasse').val(),
-                //     'motDePasseConfirmation': $('#motDePasseConfirmation').val()
-                // });
-                if (data.pseudo) {
-                    $('#inputPseudo').removeClass("successForm").remove("errorForm").addClass("successForm");
-                }
-                else {
-                    $('#inputPseudo').removeClass("successForm").remove("errorForm").addClass("errorForm");
-                }
-                if (data.mail) {
-                    $('#inputMail').removeClass("successForm").remove("errorForm").addClass("successForm");
-                }
-                else {
-                    $('#inputMail').removeClass("successForm").remove("errorForm").addClass("errorForm");
-                }
-                if (data.motDePasse) {
-                    $('#inputPassword').removeClass("successForm").remove("errorForm").addClass("successForm");
-                }
-                else {
-                    $('#inputPassword').removeClass("successForm").remove("errorForm").addClass("errorForm");
-                }
-                if (data.motDePasseConfirmation) {
-                    $('#inputPasswordConfirm').removeClass("successForm").remove("errorForm").addClass("successForm");
-                }
-                else {
-                    $('#inputPasswordConfirm').removeClass("successForm").remove("errorForm").addClass("errorForm");
-                }
-                $('#erreur ul').empty();
-                var ul = $('<ul />');
-                for (var j in data.messageErreur) {
-                    ul.append($('<li>').html(data.messageErreur[j]));
-                }
-                $('#erreur').append(ul).show();
+                $('#inputPseudo').removeClass("successForm").remove("errorForm").addClass("errorForm");
             }
+            if (data.mail) {
+                $('#inputMail').removeClass("successForm").remove("errorForm").addClass("successForm");
+            }
+            else {
+                $('#inputMail').removeClass("successForm").remove("errorForm").addClass("errorForm");
+            }
+            if (data.motDePasse) {
+                $('#inputPassword').removeClass("successForm").remove("errorForm").addClass("successForm");
+            }
+            else {
+                $('#inputPassword').removeClass("successForm").remove("errorForm").addClass("errorForm");
+            }
+            if (data.motDePasseConfirmation) {
+                $('#inputPasswordConfirm').removeClass("successForm").remove("errorForm").addClass("successForm");
+            }
+            else {
+                $('#inputPasswordConfirm').removeClass("successForm").remove("errorForm").addClass("errorForm");
+            }
+            $('#erreur ul').empty();
+            var ul = $('<ul />');
+            for (var j in data.messageErreur) {
+                ul.append($('<li>').html(data.messageErreur[j]));
+            }
+            $('#erreur').append(ul).show();
+        }
+    }).fail(erreurCritique);
+}
 
-        }).fail(erreurCritique);
-        return false;  //a garder sinon va recharger la page
+$(document).ready(function () {
+    $('#inputPseudo, #inputMail, #inputPassword, #inputPasswordConfirm, #inputNom, #inputPrenom').keypress(function (event) {
+        if (event.key === 'Enter')
+            envoyerFormulaireInscription();
     });
+    $("#boutonValider").click(envoyerFormulaireInscription);
+    return false;  //a garder sinon va recharger la page
 });
+
 
 
