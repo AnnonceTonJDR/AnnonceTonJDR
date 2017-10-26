4f9055fdb815cc7542bb592e1a11ce7459d37ea5
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-26 11:46:20 +0200

    Creation de la page register, de son lien depuis l'index, son css et son js

--- app/views/register.php
+++ app/views/register.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="/web/css/normalize.css">
+    <link rel="stylesheet" href="/web/css/register.css">
     <script src="/web/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <body>
@@ -21,13 +22,51 @@
 <div id="wrapper">
     <div id="contenu">
         <?php include_once 'app/views/utils/header.php'; ?>
-        <p>Je suis la page d'inscription</p>
+        <div id="erreur" style="display: none;">
+            Votre inscription n'a pue être réalisée, voici les erreurs:
+        </div>
+        <div style="margin-top: 20px">
+            <div id="inputPrenom">
+                <label for="prenom">Prénom : <strong style="color: red">*</strong></label>
+                <input type="text" name="prenom" id="prenom" required="">
+            </div>
+            <div id="inputNom">
+                <label for="nom">Nom : <strong style="color: red">*</strong></label>
+                <input type="text" name="nom" id="nom" required="">
+            </div>
+            <div id="inputPseudo">
+                <label for="pseudo">Pseudo : <strong style="color: red">*</strong></label>
+                <input type="text" name="pseudo" id="pseudo" required="">
+                <p class="condition">Le pseudo doit être unique et faire au minimum 4 caractères</p>
+                <!-- doit être unique taille min de 4 char-->
+            </div>
+            <div id="inputMail">
+                <label for="mail">Mail : <strong style="color: red">*</strong></label>
+                <input type="text" name="mail" id="mail" required="">
+                <p class="condition">Vous ne pouvez associer qu'un unique compte par adresse mail</p>
+            </div>
+            <div id="inputPassword">
+                <label for="motDePasse">Mot de passe : <strong style="color: red">*</strong> </label>
+                <input type="password" name="motDePasse" id="motDePasse" required="">
+                <p>Le mot de passe doit faire une taille minimale de 8 charactères, comporter une
+                    majuscule, une minuscule, et un caractère spécial</p>
+                <!-- doit contenir une majuscule, un caractère spécial, 8 caractère minimum, un chiffre -->
+            </div>
+            <div id="inputPasswordConfirm">
+                <label for="motDePasseConfirmation">Confirmer le mot de passe : <strong
+                            style="color: red">*</strong></label>
+                <input type="password" name="motDePasseConfirmation" id="motDePasseConfirmation" required="">
+                <!-- doit être le même -->
+            </div>
+            <p>Les champs suivis d'une <strong style="color: red">*</strong> sont obligatoires.</p>
+            <button id="boutonValider">Valider</button>
+        </div>
     </div>
 </div>
 
 
 <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-<script src="/web/js/plugins.js"></script>
+<script src="/web/js/site/register.js"></script>
 </body>
 </html>
---
+++ web/js/site/register.js
@@ -0,0 +1,59 @@
+/**
+ * Javascript pour le formulaire de connexion contenu dans le header
+ *
+ * @author Lucas OMS
+ */
+
+function erreurCritique() {
+    $('#contenu').html(
+        'Une erreur irrécupérable est survenue. <br />'
+        + 'Merci de contcter l\'admin <br/>'
+        + 'contact@annonceTonJDR.fr'
+    );
+}
+
+function seConnecter() {
+    $.ajax({
+        type: 'post',
+        url: '/app/controller/connexion/connexion.php',
+        data: {
+            'identifiant': $('#idConnection').val(),
+            'motDePasse': $('#pwdConnection').val()
+        }
+    }).done(function (data) {
+        if (data.ok === 1) {
+            location.reload(true);
+        }
+        else if (data.ok === -1) {
+            $('#pwdConnection').css({'background': 'rgb(200,25,25)'});
+        }
+        else if (data.ok === -2) {
+            $('#idConnection').css({'background': 'rgb(200,25,25)'});
+        }
+    }).fail(erreurCritique);
+}
+
+$(document).ready(function () {
+    $("#connectionButton").click(function () {
+        seConnecter();
+    });
+
+    $('#idConnection').keypress(function (event) {
+        if (event.key === 'Enter')
+            seConnecter();
+    });
+
+    $('#pwdConnection').keypress(function (event) {
+        if (event.key === 'Enter')
+            seConnecter();
+    });
+
+    $('#deconnectionButton').click(function () {
+        $.ajax({
+            type: 'get',
+            url: '/app/controller/connexion/deconnexion.php'
+        }).done(function () {
+            location.reload(true);
+        }).fail(erreurCritique);
+    });
+});
