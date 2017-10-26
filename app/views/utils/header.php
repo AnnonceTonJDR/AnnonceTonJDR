d093b2d450b605ed2ef7c90d9ea3a872630bfa1d
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-26 08:27:06 +0200

    Ajout des liens vers les scripts necessaires au header

--- app/views/utils/header.php
+++ app/views/utils/header.php
@@ -1,3 +1,7 @@
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="/web/js/site/header.js"></script>
+
 <div id="menuConnexion">
     <div class="formulaireConnexion">
         <label for="idConnection">Identifiant</label><input type="text" name="identifiantConnexion"
