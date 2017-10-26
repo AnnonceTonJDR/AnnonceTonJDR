98ad6186734a5578f735ff581087fbb2eb66fa82
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-26 08:18:02 +0200

    Ajout du header de base

---
+++ app/views/utils/header.php
@@ -0,0 +1,11 @@
+<div id="menuConnexion">
+    <div class="formulaireConnexion">
+        <label for="idConnection">Identifiant</label><input type="text" name="identifiantConnexion"
+                                                            id="idConnection"><br>
+        <label for="pwdConnection">Mot de passe</label><input type="password" name="motDePasseConnexion"
+                                                              id="pwdConnection"><br>
+        <button id="connectionButton">Se connecter</button>
+        <a id="forgottenPswd" href="">Mot de passe oublié ?</a>
+        <a id="signUp" href="">S'inscrire</a>
+    </div>
+</div>
