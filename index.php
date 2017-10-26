86a41d445afb52cadbe8f9bf05697b84f3ecdfed
Lucas OMS <lucas.oms@etu.unice.fr>
2017-10-26 09:06:38 +0200

    Format code

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php
-    session_start();
-    /*
-        Ficher de routing
-    */
+session_start();
+/*
+    Ficher de routing
+*/
 
-    // Register
-    if (isset($_GET["page"]) && $_GET["page"] === "register")
-    {
-        include_once 'app/controller/register.php';
-    }
-    // CGU
-    elseif (isset($_GET["page"]) && $_GET["page"] === "cgu")
-    {
-        include_once 'app/views/conditionsUtilisation.php';
-    }
-    // Profil l'utilisateur
-    elseif (isset($_GET["page"]) && $_GET["page"] === "user")
-    {
-        include_once 'app/controller/user.php';
-    }
-    // Index
-    else
-    {
-        include_once 'app/controller/index.php';
-    }
+// Register
+if (isset($_GET["page"]) && $_GET["page"] === "register") {
+    include_once 'app/controller/register.php';
+} // CGU
+elseif (isset($_GET["page"]) && $_GET["page"] === "cgu") {
+    include_once 'app/views/conditionsUtilisation.php';
+} // Profil l'utilisateur
+elseif (isset($_GET["page"]) && $_GET["page"] === "user") {
+    include_once 'app/controller/user.php';
+} // Index
+else {
+    include_once 'app/controller/index.php';
+}
