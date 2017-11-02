5d7ee437404a332ec45b6ca75c62c054c391abd5
Lucas OMS <lucas.oms@etu.unice.fr>
2017-11-02 14:49:21 +0100

    Retrait de commentaires inutiles

--- web/js/site/forgottenPsw.js
+++ web/js/site/forgottenPsw.js
@@ -37,22 +37,6 @@ function resetMDP() {
 
 $(document).ready(function () {
 
-    // $('#btnDemande').click(function () {
-    //     $.ajax({
-    //         type: 'get',
-    //         url: 'app/controller/connexion/sendForgottenPWD.php?identifiant=' + $('#identifiant').val()
-    //     }).done(function (data) {
-    //         if (data.ok) {
-    //             code = data.code;
-    //             $('#demandeReinitialisation').slideUp('slow');
-    //             $('#codeReinitialisation').slideDown('slow');
-    //         }
-    //         else {
-    //             $('#erreurIdentifiant').html(data.message).show();
-    //         }
-    //
-    //     }).fail(erreurCritique);
-    // });
 
     $('#sendCodeButton').click(function () {
         mail = $("#id").val();
@@ -90,27 +74,6 @@ $(document).ready(function () {
         });
     });
 
-// $('#btnReinitialisation').click(function () {
-//     $.ajax({
-//         type: 'get',
-//         url: '../../controler/connexion/resetPwdWithCode.php?mdp=' + $('#motDePasseReset').val() + '&confirm=' + $('#motDePasseConfirmation').val() + '&log=' + $('#identifiant').val()
-//     }).done(function (data) {
-//         if (data.ok) {
-//             $('#succesPassword').slideDown('slow');
-//             $('#erreurPassword').slideUp('slow');
-//             $('#reinitialisation').slideUp('slow');
-//         }
-//         else {
-//             $('#erreurPassword ul').empty();
-//             var ul = $('<ul />');
-//             for (var j in data.message) {
-//                 ul.append($('<li>').html(data.message[j]));
-//             }
-//             $('#erreurPassword').append(ul).show();
-//         }
-//
-//     }).fail(erreurCritique);
-// })
 });
 
 
