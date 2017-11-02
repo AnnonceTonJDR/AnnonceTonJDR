8842d1a1cf862dc520f217a9f1b87c905002fdec
Lucas OMS <lucas.oms@etu.unice.fr>
2017-11-03 00:11:23 +0100

    Ajout du JS d'oublie de mot de passe

--- web/js/site/forgottenPwd.js
+++ web/js/site/forgottenPwd.js
@@ -1,54 +1,81 @@
+var mail;
+
+
+function envoyerCode() {
+    mail = $("#id").val();
+    //TODO envoi du mail avec le code
+    $.ajax({
+        type: 'get',
+        url: 'app/controller/connexion/sendResetPwdCode.php?identifiant=' + mail
+    }).done(function (data) {
+        if (data.ok) {
+            $('#askingReset').slideUp('quick', function () {
+                $('#enterCode').slideDown('down');
+            });
+        }
+        else {
+            alert("Mail invalide");
+        }
+
+    }).fail(erreurCritique);
+}
+
+function validateCode() {
+    //TODO vérifie le code sur la BD
+    $('#enterCode').slideUp('quick', function () {
+        $('#reset').slideDown('down');
+    });
+}
+
+function resetMDP() {
+    //TODO ajax avec mail, code, et new mdp /mdp confirm
+    //TODO renvoie oui ou non (et si erreur, affichage)
+    $('#reset').slideUp('quick', function () {
+        $('#succesPassword').slideDown('down');
+    });
+}
+
 $(document).ready(function () {
-    var code;
 
-    $('#btnDemande').click(function () {
-        $.ajax({
-            type: 'get',
-            url: 'app/controller/connexion/sendForgottenPWD.php?identifiant=' + $('#identifiant').val()
-        }).done(function (data) {
-            if (data.ok) {
-                code = data.code;
-                $('#demandeReinitialisation').slideUp('slow');
-                $('#codeReinitialisation').slideDown('slow');
-            }
-            else {
-                $('#erreurIdentifiant').html(data.message).show();
-            }
+    $("#id").keypress(function (event) {
+        if (event.key === 'Enter')
+            envoyerCode();
+    });
 
-        }).fail(erreurCritique);
+    $('#sendCodeButton').click(function () {
+        envoyerCode();
+    });
+
+    $('#hasCodeAlreadyButton').click(function () {
+        mail = $("#mail").val();
+        $('#askingReset').slideUp('quick', function () {
+            $('#askingMail').slideDown('down');
+        });
+    });
+
+    $('#validateMail').click(function () {
+        $('#askingMail').slideUp('quick', function () {
+            $('#enterCode').slideDown('down');
+        });
+    });
+
+    $("#codeReinit").keypress(function (event) {
+        if (event.key === 'Enter')
+            envoyerCode();
     });
 
     $('#btnCode').click(function () {
-        console.log(code);
-        console.log($('#codeReinit').val());
-        if ($('#codeReinit').val() == code) {
-            $('#codeReinitialisation').slideUp('slow');
-            $('#reinitialisation').slideDown('slow');
-        }
+        validateCode();
     });
 
-    $('#btnReinitialisation').click(function () {
-        $.ajax({
-            type: 'get',
-            url: '../../controler/connexion/resetMDP.php?mdp=' + $('#motDePasseReset').val() + '&confirm=' + $('#motDePasseConfirmation').val() + '&log=' + $('#identifiant').val()
-        }).done(function (data) {
-            if (data.ok) {
-                $('#succesPassword').slideDown('slow');
-                $('#erreurPassword').slideUp('slow');
-                $('#reinitialisation').slideUp('slow');
-            }
-            else {
-                $('#erreurPassword ul').empty();
-                var ul = $('<ul />');
-                for (var j in data.message) {
-                    ul.append($('<li>').html(data.message[j]));
-                }
-                $('#erreurPassword').append(ul).show();
-            }
+    $("#motDePasseReset, #motDePasseConfirmation").keypress(function (event) {
+        if (event.key === 'Enter')
+            resetMDP();
+    });
 
-        }).fail(erreurCritique);
-    })
-
+    $('#validateNewPwd').click(function () {
+        resetMDP();
+    });
 });
 
 
