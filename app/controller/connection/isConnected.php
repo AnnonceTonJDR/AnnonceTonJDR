cc1b2455c0c0d0f9ea5817a9654b40e26e87bc48
Lucas OMS <lucas.oms@hotmail.fr>
2017-11-11 01:07:13 +0100

    Traduction isConnected.php

--- app/controller/connection/isConnected.php
+++ app/controller/connection/isConnected.php
@@ -7,21 +7,21 @@
 session_start();
 require_once '../../model/Utilisateur.php';
 require_once '../../model/session.php';
-$retour = false;
+$returnValue = false;
 
 /************************************************************************
  *Si une session est initialisée c'est qu'un utilisateur est connecté et on récupère son id    *
  *************************************************************************/
 if (isset($_SESSION['session'])) {
     $id = unserialize($_SESSION['session'])->getUtilisateur()->getId();
-    $retour = true;
+    $returnValue = true;
 }
 
 /**************************************
  *On renvoie le drapeau et l'id de l'utilisateur        *
  ***************************************/
 $obj = new stdClass();
-$obj->ok = $retour;
+$obj->ok = $returnValue;
 $obj->id = $id;
 
 header('Cache-Control: no-cache, must-revalidate');
