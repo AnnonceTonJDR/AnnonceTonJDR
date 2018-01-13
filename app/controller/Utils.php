4abe42deee9cbc4b9346246f450552be566d873e
Lucas OMS <lucas.oms@hotmail.fr>
2018-01-14 00:04:07 +0100

    Ajout PHP7 sur Utils

--- app/controller/Utils.php
+++ app/controller/Utils.php
@@ -2,7 +2,7 @@
 
 class Utils
 {
-    public static function isValidPwd($password): bool
+    public static function isValidPwd(string $password): bool
     {
         //Si on veut rendre plus compliqué un jour
         //preg_match('#[A-Z]#', $_POST['motDePasse']) < 1 || preg_match('#[0-9]#', $_POST['motDePasse']) < 1 || preg_match('/[^a-zA-Z0-9]+/', $_POST['motDePasse'] < 1))
