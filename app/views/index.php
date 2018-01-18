fe841e1ae565bf8bf539366f32cc03d57eaa4f42
Lucas OMS <lucas.oms@hotmail.fr>
2018-01-18 02:51:41 +0100

    Changement de l'index pour la nouvelle présentation (parchemin)

--- app/views/index.php
+++ app/views/index.php
@@ -10,8 +10,10 @@ session_start();
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/png" href="/image/logo.png"/>
+
     <link rel="stylesheet" href="/web/css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="/web/css/laptop.css">
+    <link rel="stylesheet" type="text/css" href="/web/css/index.css">
     <link rel="stylesheet" type="text/css" href="/web/css/mobile.css">
     <script src="/web/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
@@ -25,41 +27,34 @@ session_start();
 <!-- Add your site or application content here -->
 <div id="wrapper">
     <div id="contenu">
-      <div class="row">
-        <div class="col-2">
-          <div id="div2">
-            <p>&nbsp;</p>
-          </div>
-        </div>
-        <div class="col-8 col-md-12 ">
-          <div id="div1">
-            <?php include_once 'app/views/utils/header.php'; ?>
-            <br>
-            <br>
-            <h1> Annonce ton JDR </h1>
-            <br>
-            <br>
-            <h2> Recherche de partie </h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                non proident, sunt in culpa qui officia deserunt mollit anim id est
-                laborum.</p>
-          </div>
-        </div>
-        <div class="col-2">
-        <div id="div2">
-          &nbsp;
-        </div>
-      </div>
-</div>
-
+        <div class="leftBorder"></div>
+        <div class="rightBorder"></div>
+        <?php include_once 'app/views/utils/header.php'; ?>
+        <br>
+        <br>
+        <h1> Annonce ton JDR </h1>
+        <br>
+        <br>
+        <h2> Recherche de partie </h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.</p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.</p>
         <p>Je suis la page d'index</p>
+        <?php include_once 'app/views/utils/endScroll.php'; ?>
     </div>
+    <?php include_once 'app/views/utils/footer.php'; ?>
 </div>
-
 
 <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
