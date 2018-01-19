e18baa9c6efc6cf55713c4a5c206be4f517bdb22
Lucas OMS <lucas.oms@etu.unice.fr>
2018-01-19 07:36:47 +0100

    Utilisation de la fonction de début de page

--- app/views/conditionsUtilisation.php
+++ app/views/conditionsUtilisation.php
@@ -1,32 +1,7 @@
-<!doctype html>
-<html class="no-js" lang="">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>AnnonceTonJDR - CGU</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-    <!-- Place favicon.ico in the root directory -->
-
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/presentationAccueil.css">
-    <link rel="stylesheet" href="/css/vitrine.css">
-    <link rel="stylesheet" href="/css/conditionUtilisation.css">
-    <script src="/js/vendor/modernizr-2.8.3.min.js"></script>
-</head>
-<body>
-<!--[if lt IE 8]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
-        href="http://browsehappy.com/">upgrade
-    your browser</a> to improve your experience.</p>
-<![endif]-->
-
-<!-- Add your site or application content here -->
-<div id="wrapper">
-    <?php include_once 'utils/accueilHeader.php' ?>
-    <div id="contenu" class="CGU">
+<?php
+require_once 'utils/start_end_page.php';
+startPage("Annonce ton JDR - CGU");
+?>
         <h1 id="CGU">Conditions générales d'utilisations - AnnonceTonJDR</h1>
         <div class="titre">
             <img src="/img/site/titreCGU.png">
