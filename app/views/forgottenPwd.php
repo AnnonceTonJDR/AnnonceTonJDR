b272b9cc3ade902425f216154dffe1f863f5a1cd
Lucas OMS <lucas.oms@etu.unice.fr>
2018-01-19 07:52:36 +0100

    Mise en page titre et boutons

--- app/views/forgottenPwd.php
+++ app/views/forgottenPwd.php
@@ -3,9 +3,7 @@ require_once 'utils/start_end_page.php';
 startPage("Annonce ton JDR - Oubli de mot de passe");
 ?>
     <div id="askingReset" class="panel panel-danger">
-        <div>
-            <strong>Quel est votre mail ?</strong>
-        </div>
+        <h3>Quel est votre mail ?</h3>
         <div>
             <div>
                 Un mail vous sera envoyé avec un code pour changer votre mot de passe.
