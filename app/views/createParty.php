e569992e3b8cadaa9fd2b81203d1aaa1328e85ba
Lucas OMS <lucas.oms@hotmail.fr>
2018-01-22 21:59:37 +0100

    Ajout champ adresse

--- app/views/createParty.php
+++ app/views/createParty.php
@@ -7,9 +7,20 @@ require_once 'app/model/session.php';
 startPage("Annonce ton JDR - Créer une partie", ["createParty.css"], ["site/createParty.js"]);
 $user = Session::unserializeConnectedUser();
 ?>
-    <p id="addressText"><label for="adresse">J'organiserais mon jeu de rôle en ce lieu : </label><input type="text"
-                                                                                                        id="adresse"/>.
+    <p id="addressText"><label for="adresse">J'organiserais mon jeu de rôle en ce lieu : </label>
+        <input type="text" style="min-width: 200px;" id="place-input">.
     </p>
+
+    <!--    la library javascript avec notre apiKey-->
+    <script type="text/javascript"
+            src="//maps.googleapis.com/maps/api/js?key=AIzaSyDtnql0_LAPbI6QU8GTnlShmyJ7QQMSL1Q&libraries=places"></script>
+    <!--    initialisation du field-->
+    <script defer='defer' type='text/javascript'>
+        var input = document.getElementById('place-input');
+        autocomplete = new google.maps.places.Autocomplete(input);
+    </script>
+
+
     <p><input type="checkbox" id="isVirtual"/><label for="isVirtual">Cette partie se déroulera sur le net.</label></p>
     <!--region nombre de joueur-->
     <p><label for="joueurMax">Via cette annonce, je cherche à recruter</label><input type="number" min="1" max="99"
@@ -55,58 +66,6 @@ $user = Session::unserializeConnectedUser();
         <textarea id="commentaire"></textarea>
     </p>
     <!--endregion-->
-
-    <!--TODO addresse-->
-    <!-- html -->
-    <div class="mdl-textfield mdl-js-textfield mdl-cell">
-        <input type="text" class="mdl-textfield__input control" id="place-input" placeholder="Où habitez vous">
-    </div>
-    <!-- javascript-->
-<!--    la library javascript avec notre apiKey-->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDtnql0_LAPbI6QU8GTnlShmyJ7QQMSL1Q&libraries=places"></script>
-<!--    initialisation du field-->
-    <script defer='defer' type='text/javascript'>
-        var input = document.getElementById('place-input');
-
-        autocomplete = new google.maps.places.Autocomplete(input);
-    </script>
-   <!--
-    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNWWWWWNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNX0kkkkxxkOkkkk0KXXKXNMMMMMMMMMMMMMMMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMMMMMMMMMMMWWWNXX0Okxoc:ccllldolooooocok0KXXNWMMMMMMMMMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMMMMMMMMWNXKKKKK00OOOOxddxdoodollcc::cloollodOKNMMMMMMMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMMMMMWNXXK0000KK0O0OOOOOkxkkxdollloooxkxxdddxkxkKNWMMMMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMMMWNXK00OOO0KKKK0KKOkxxkkxxdooolodxdxkOOkkkkOOkxO0XNWMMMMMMMMMMM
-    MMMMMMMMMMMMMMMMWNKOxooodxxxxkKKKXK0000OOOkkkxxxkkkxxxkOkxxxxxddxxxxONMMMMMMMMMM
-    MMMMMMMMMMMMMWWNX0o;cdOKNNNX0dd0XXKKXXXKKK000OO00000OO0OkxddoooddddddOKWMMMMMMMM
-    MMMMMMMMMMMMNK0KOc..,;cdxkkdllcdKXXXNNNNNNNNXXKK00XKKK0Okdodxolc::loxxx0NMMMMMMM
-    MMMMMMMMMMWX0O0x,.      ...  .,oKXXNNNNNWWWWWNNNXXNXXX0OddOXWX0xc,':oodx0NMMMMMM
-    MMMMMMMMWNKkk0Oc'.           .,xKXNNNNNNWWWWWNNNNNNNNK0dclolc;'.....;cclxKWMMMMM
-    MMMMMMMNKOxxkKk;..           .oOKXNNWNNWWNWWWNNWWNNNXK0o;.           .;:okXWMMMM
-    MMMMMMN0kddx0Kkc'.  .    ...,o00KXNNNNWNNNWWWNNWNNNNXKOl,.           ..;cokKWMMM
-    MMMMMNOkxxkkOKKx:'........;ok00KXXNNNNNNNNWNNNNNNNNXXX0o,.           ..;:clokXWM
-    MMMMN0kxkxxxk0KKko:;,;:ldk0KKKKXNNNNNNNNNNNNWWNNNNNNXK0kl,.          .';:clllxKW
-    MMWXOkxxxdxxxxkO00OOOO0KXXNNNNWWWWWNXXNNNNWWWWWWNNXXXK00ko;...     ..',::::cllok
-    MWXkdddddxxxdxkkkkkOOOKXNWWWWWWWWWNKkxk0XXNWWNX0OOKKKKK00kdc;'......,::::::;::cl
-    MNOdddxxxxdloddddxO0KXNWWWWWWWWWWWWNNX00KXNNXKOkxk00KKKK0Okdolc;;;:cc:;;;;;:;:::
-    WKdcclol:;;cllldk0XNNNWNWWWWWWWWWWNNXXXXXXKKKXXXXXXXXXXKK0Okdoolc::;,;:::;,,;:c:
-    WOc:cll:;;::cox0KXNNNNNNNNNNNNNNNNX0kdlcc::clok0KKXNXKK000OOxdlc;,,,';cc::;,,;::
-    Xkolllooolldk0KXXXNNNNNNNNNNNNNX0dllldxxdxxdolllok0KKK0OOkkkxdlc;,'''.,;;,,;,,::
-    dodoodoolok0KXXXXXXXXXNNNNXXXX0xllx0KXKKK00000Oxocok00Okkkkxdool;......',;;;;;;:
-    :cooolcloxOKXXXXXXXXXXXXXXKK0xdx0XXKKK0000OOkkkkkdclk0Okxxxxdllcc;.....',,,;:::c
-    ;:cccllodOKXXXXXXXXKKKKKKK0OxdOXXXXXXXXKKKK000000Oxodkkxdooolllooo;.........';;:
-    ;;;;codoxOKKKXXXXXXKK0000Okxk0XXXXXXXXXKKKKKKKKK000xlldddooollodxdo,..........',
-    :;::cloodkO0KKKKKK000OOkkkkOKKKKKKXXXXKKKKKKKXKKK0Okdloxxdlllloddxxd:...........
-    c:cccooooxO0000000OOOkkxxkO0000KKKKKKKK00KK0KKKK0Okkxoldxooollooooxxo:,.....'.''
-    lcc::cloodkO0000OOkkxddooxkO0000000000O0000OO000Okxxdlldxxxdllooooddol;,''''....
-    clc;,;;:loxxxkkkxddooooodkOO0OOOOOOOOOOOOOOkkkOkkxdolccdkkxdoodddooolc;,''''....
-    cc:;;,;:odddodddxddxkkkkOO00OOOOOOOOOOOOOkkxddddddoc:::ldxddxxddollllc;,........
-    :::::::coodoooxkkkOO000KKKK000000000OOOkkkxdoooooolcclllloddxxddlccc:;,'........
-    :cc::ccldddddxO000KKKKKXXXXKKKXXKKK0000OOOkxxxddxxxxddooooloooolc;;::,,''.......
-    ;:::clldddxkOO00KKKKXXXNNNXXXXXXKKKK0000000OOOkkOOOOkxdooolllll:,',;;;,'''......
-    -->
-
 
     <!--region A amener/possibilité de fumer -->
     <p><label for="typeLieu">Nous jouerions</label>
